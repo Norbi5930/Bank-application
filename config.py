fac92a3075a3f9a99bd5b62fcfa9968ff5f1f077
Norbi5930 <145148532+Norbi5930@users.noreply.github.com>
2023-11-29 14:20:07 +0100

    bugfix

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "bnorbert0925@gmail.com"
-app.config["MAIL_PASSWORD"] = "qalq xsmh qcud lkml"
+app.config["MAIL_USERNAME"] = "example@gmail.com"
+app.config["MAIL_PASSWORD"] = "password"
